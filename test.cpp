715dd8e8c431933173a3ce57a53a37e8db5d39af
wa9head <wa9head@gmail.com>
2021-10-18 21:55:11 +0300

    vector1

--- test.cpp
+++ test.cpp
@@ -1,18 +1,21 @@
 
-
-
+#include <vector>
+#include <iterator>
 #include <iostream>
 //how does this not segfault????
 int main() {
 	std::allocator<int> alloc;
-	int* pointer = alloc.allocate( 5 );
-	pointer[0] = 1;
-	pointer[1] = 2;
-	pointer[2] = 3;
-	pointer[3] = 4;
-	pointer[4] = 5;
-	pointer[5] = 6;
-	for(int i = 0; i < 6; i++) {
+	std::vector<int> pointer;
+
+
+	
+	pointer.push_back(1);
+	pointer.push_back( 2 );
+	pointer.push_back( 3 );
+	for(int i = 0; i < 100; i++) {
 		std::cout << pointer[i] << std::endl;
 	}
+
+
+	
 }
--- vector.hpp
+++ vector.hpp
@@ -1,5 +1,60 @@
 #pragma once
 
+#include <memory>
+#include <cstddef>
+#include <iterator>
+namespace ft {
+	template <class T, class Allocator = std::allocator<T> >
+	class vector {
+		typedef T				value_type;
+		typedef Allocator		allocator_type;
+		typedef std::size_t		size_type;
+		typedef std::ptrdiff_t	difference_type;
+		typedef value_type& reference;
+		typedef const value_type& const_reference;
+		typedef typename Allocator::pointer	pointer;
+		typedef typename Allocator::const_pointer const_pointer;
+		typedef typename std::iterator < std::random_access_iterator_tag, value_type > iterator;
+		typedef typename std::iterator < std::random_access_iterator_tag, const value_type > const_iterator; //is it though???
+		typedef typename std::reverse_iterator<iterator> reverse_iterator;
+		typedef typename std::reverse_iterator<const_iterator> const_reverse_iterator;
 
-template <class T>
+		value_type* _data;
+	public:
+		//default constructor
+		explicit vector( const allocator_type& alloc = allocator_type() ) {
+			_data = alloc.allocate( 0 );
+		}
 
+		//fill constructor
+		explicit vector( size_type n, const value_type& val = value_type(),
+			const allocator_type& alloc = allocator_type() ) {
+			_data = alloc.allocate( n );
+			for(size_type i = 0; i < n; i++) {
+				_data[i] = val;
+			}
+		}
+
+		//range constructor
+		template <class InputIterator>
+		vector( InputIterator first, InputIterator last,
+			const allocator_type& alloc = allocator_type() ) {
+			_data = alloc.allocate( static_cast<size_type> (last - first) );
+			size_type j = 0;
+			for(InputIterator i = first; i != last; i++) {
+				*i = _data[j];
+				j++;
+			}
+		}
+
+		//copy constructor <--------NYI
+		vector( const vector& x ) {
+			if(this != &x) {
+
+			}
+		}
+		virtual ~vector() {
+			
+		}
+	};
+}
